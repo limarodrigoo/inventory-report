996c191597ee38c1c04d40dd14ccad52ce2cc920
Rodrigo Lima <limarodrigoo@outlook.com>
2022-07-14 15:23:19 -0300

    feat create report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    coke = Product(
+        28,
+        "Coca Cola",
+        "Coca Cola",
+        "12/03/22",
+        "12/03/23",
+        "123456",
+        "Reservar gelado",
+    )
+
+    assert str(coke) == (
+        "O produto Coca Cola fabricado em 12/03/22 "
+        "por Coca Cola com validade até 12/03/23 "
+        "precisa ser armazenado Reservar gelado"
+    )
