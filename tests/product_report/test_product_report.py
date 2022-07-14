07761503cbd6677ac3036207cdd965fb3a1453a6
Rodrigo Lima <limarodrigoo@outlook.com>
2022-07-14 15:44:06 -0300

    update product report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -3,18 +3,27 @@ from inventory_report.inventory.product import Product
 
 def test_relatorio_produto():
 
+    id = 28
+    product_name = "Coca Cola"
+    product_company = "Coca Cola"
+    manufactured = "12/03/22"
+    best_before = "12/03/23"
+    serial_number = "123456"
+    storage_instructions = ("Reservar gelado",)
     coke = Product(
-        28,
-        "Coca Cola",
-        "Coca Cola",
-        "12/03/22",
-        "12/03/23",
-        "123456",
-        "Reservar gelado",
+        id,
+        product_name,
+        product_company,
+        manufactured,
+        best_before,
+        serial_number,
+        storage_instructions,
     )
 
     assert str(coke) == (
-        "O produto Coca Cola fabricado em 12/03/22 "
-        "por Coca Cola com validade até 12/03/23 "
-        "precisa ser armazenado Reservar gelado"
+        f"O produto {product_name}"
+        f" fabricado em {manufactured}"
+        f" por {product_company} com validade"
+        f" até {best_before}"
+        f" precisa ser armazenado {storage_instructions}."
     )
