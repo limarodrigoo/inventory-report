7111d0d24f3da7b7814adb093496bc6f4af24539
Rodrigo Lima <limarodrigoo@outlook.com>
2022-07-14 17:10:13 -0300

    fix python syntax

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-import datetime
+from datetime import datetime, date
 
 
 class SimpleReport:
@@ -13,22 +13,24 @@ class SimpleReport:
         for product in data:
             fabrication_date.append(
                 datetime.strptime(
-                    product.data_de_fabricacao, "%Y/%m/%d"
+                    product["data_de_fabricacao"], "%Y-%m-%d"
                 ).date()
             )
             best_before_date.append(
-                datetime.strptime(product.data_de_validade, "%Y/%m/%d").date()
+                datetime.strptime(
+                    product["data_de_validade"], "%Y-%m-%d"
+                ).date()
             )
-            companies.append(product.nome_da_empresa)
+            companies.append(product["nome_da_empresa"])
 
-        most_common_company = Counter(companies).most_common(1)(0)
+        most_common_company = Counter(companies).most_common(1)[0][0]
 
-        for date in best_before_date:
-            if date < datetime.date.today():
+        for product_date in best_before_date:
+            if product_date < date.today():
                 best_before_date.remove(date)
 
         return (
-            f"Data de fabricação mais antiga: {min(fabrication_date)}\n"
-            f"Data de validade mais próxima: {max(best_before_date)}\n"
-            f"Empresa com mais produtos: {most_common_company}\n"
+            f'Data de fabricação mais antiga: {min((fabrication_date))}\n'
+            f'Data de validade mais próxima: {min((best_before_date))}\n'
+            f'Empresa com mais produtos: {most_common_company}'
         )
