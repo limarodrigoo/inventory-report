feafe826877bcd2c214e862fea89009dd1185631
Rodrigo Lima <limarodrigoo@outlook.com>
2022-07-14 21:09:31 -0300

    update add json type of import data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+from json import load
 from ..reports.complete_report import CompleteReport
 from ..reports.simple_report import SimpleReport
 
@@ -6,12 +7,21 @@ from ..reports.simple_report import SimpleReport
 class Inventory:
     @staticmethod
     def import_data(path: str, type: str):
-        with open(path, encoding="utf-8") as file:
-            csv_report = csv.DictReader(file)
-            csv_list = list(csv_report)
-            if type == "simples":
-                return SimpleReport.generate(csv_list)
-            elif type == "completo":
-                return CompleteReport.generate(csv_list)
-            else:
-                raise ValueError
+        if "csv" in path:
+            with open(path, encoding="utf-8") as file:
+                csv_report = csv.DictReader(file)
+                csv_list = list(csv_report)
+                return Inventory.generate_report(csv_list, type)
+        if "json" in path:
+            with open(path, encoding="utf-8") as file:
+                json_list = load(file)
+                return Inventory.generate_report(json_list, type)
+
+    @staticmethod
+    def generate_report(list, type):
+        if type == "simples":
+            return SimpleReport.generate(list)
+        elif type == "completo":
+            return CompleteReport.generate(list)
+        else:
+            raise ValueError
