3218e3edb1ae0f45db09dcc3595437c07e282fa4
Rodrigo Lima <limarodrigoo@outlook.com>
2022-07-14 21:50:48 -0300

    update add feature xml type for import data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ import csv
 from json import load
 from ..reports.complete_report import CompleteReport
 from ..reports.simple_report import SimpleReport
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -16,6 +17,12 @@ class Inventory:
             with open(path, encoding="utf-8") as file:
                 json_list = load(file)
                 return Inventory.generate_report(json_list, type)
+        if "xml" in path:
+            with open(path) as file:
+                xml_reader = ET.parse(path)
+                xml_dom_elements = xml_reader.getroot()
+                xml_list = Inventory.generate_xml_list(xml_dom_elements)
+                return Inventory.generate_report(xml_list, type)
 
     @staticmethod
     def generate_report(list, type):
@@ -25,3 +32,14 @@ class Inventory:
             return CompleteReport.generate(list)
         else:
             raise ValueError
+
+    @staticmethod
+    def generate_xml_list(xml):
+        xml_list = []
+
+        for elem in xml.findall("./record"):
+            product_info = {}
+            for product in elem:
+                product_info[product.tag] = product.text
+            xml_list.append(product_info)
+        return xml_list
